4d997f5a4413e0ecf3150464061e349a624b6678
lgzhang <lgzhang08@gmail.com>
2014-08-26 13:14:50 +0800

    ng-demo 0.0.0

---
+++ app/js/directives/app/container.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('ng-demo').directive('appContainer', function() {
+  return {
+    restrict: 'EA',
+    templateUrl: 'views/home/container.html',
+    controller: function(){
+
+    },
+
+    
+    controllerAs: 'container'
+
+  }
+});
